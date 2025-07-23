276f9dc07e4bec5cef82397ca40867891305b9c8
laluridhoo <ridhobingkir@gmail.com>
2025-07-23 16:41:14 +0800

    menambahkan aiRoutes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bookRoutes = require("./routes/bookRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const userRoutes = require("./routes/userRoutes");
-
+const aiRoutes = require("./routes/aiRoutes");
 app.use(express.json());
 app.use("/api/books", bookRoutes);
 app.use(errorHandler);
 app.use("/api/users", userRoutes);
+app.use("/api/ai-assistant", aiRoutes);
 
 module.exports = app;
