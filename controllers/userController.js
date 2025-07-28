cb66ed0e30d05a65b24287824634213a452e5ed9
laluridhoo <ridhobingkir@gmail.com>
2025-07-28 20:43:38 +0800

    menyesuaikan format respon login

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,11 +40,17 @@ exports.loginUser = async (req, res) => {
     return res.status(401).json({ message: "Invalid email or password" });
   }
 
-  res.json({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    token: generateToken(user._id),
+  res.status(200).json({
+    success: true,
+    message: "Login successful",
+    data: {
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      token: generateToken(user._id),
+    },
   });
 };
 
