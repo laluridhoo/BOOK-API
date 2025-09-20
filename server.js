fc4d5fe8a8d077ef39fe30f8d2e470b728feee49
laluridhoo <ridhobingkir@gmail.com>
2025-09-20 11:03:48 +0800

    memindahkan dotenv kebagian paling atas

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const dotenv = require("dotenv");
+dotenv.config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-dotenv.config();
 connectDB();
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
